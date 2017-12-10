c09c3e9429d0cafdca2c172232b4b9ce3438186a
slawso12 <slawso12@gmail.com>
2017-12-10 12:31:23 -0700

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,31 @@
+package com.stephonlawson.helloworld;
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) { 
+		boolean itsSunny = true;
+		boolean atBeach = true;
+		
+		if (itsSunny != true) {
+		System.out.println("Wear Sunglasses!"); 
+		}
+		
+		if (atBeach == true) {
+			System.out.println("Wear Sunblock!");
+		}
+		
+		if(atBeach == true) {
+			System.out.println("Come back tomorrow.");
+		}
+		
+		else {
+			System.out.println("Don't need sunblock!");
+			System.out.println("Do not wear sunglasses.");
+		}
+
+
+	}	
+}
+     
+     
